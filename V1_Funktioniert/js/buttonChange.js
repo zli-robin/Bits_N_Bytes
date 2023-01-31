6290743f15c01fd7822331b4f8da6b3e9e6ba99f
Samuel Muggli <118166453+Uruguruu@users.noreply.github.com>
2023-01-31 16:03:43 +0100

    better dropdown menu

---
+++ V1_Funktioniert/js/buttonChange.js
@@ -0,0 +1,14 @@
+function changeImageAPI() {
+    if (document.getElementById("imgClickAndChangeAPI").src == ""){
+        document.getElementById("imgClickAndChangeAPI").src = "";
+    } else {
+        document.getElementById("imgClickAndChangeAPI").src = "";
+    }
+}
+function changeImagePLA() {
+    if (document.getElementById("imgClickAndChangePLA").src == ""){
+        document.getElementById("imgClickAndChangePLA").src = "";
+    } else {
+        document.getElementById("imgClickAndChangePLA").src = "";
+    }
+}
