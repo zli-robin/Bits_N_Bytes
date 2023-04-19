5d6869d5167f11f3cb8efd7d05e467d6b433de43
Robin <bluenight0696@gmail.com>
2023-04-19 14:16:09 +0200

    script t1

---
+++ Docker.ps1
@@ -0,0 +1,12 @@
+﻿# Öffne die URL im Standardbrowser
+Start-Process "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe"
+
+# Wechsle in das Download-Verzeichnis
+cd $env:userprofile\Downloads
+
+# Fordere den Benutzer auf, den Download abzuschließen
+Read-Host "Wenn Docker Desktop Installer heruntergeladen ist, drücke Enter"
+
+# Führe die beiden Befehle aus
+Start-Process -Wait "Docker Desktop Installer.exe" -ArgumentList "install"
+wsl --install
